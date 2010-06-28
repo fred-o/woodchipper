e38c073e6adb370e51331b0b97f39ec951bd9c52
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-06-28 16:26:51 +0200

    Reorg!

---
+++ woodchipper-test-jar/src/main/java/TestClass2.java
@@ -0,0 +1,12 @@
+package woodchipper;
+
+
+import woodchipper.log4j.Logger;
+
+public class TestClass2 {
+	
+	public static void main(String[] argv) {
+		Logger.getLogger(TestClass2.class).info("hello, world!");
+	}
+
+}
---
+++ woodchipper/src/main/java/woodchipper/JarProcessor.java
@@ -0,0 +1,14 @@
+package woodchipper;
+
+
+import java.io.File;
+
+public class JarProcessor {
+    
+	
+
+	public JarProcessor(File input, File output) {
+		
+	}
+
+}
