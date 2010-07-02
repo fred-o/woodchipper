f7d9b776d50cf3c66fb2673406c2d85f38d31206
Fredrik Appelberg <fredrik.appelberg@gmail.com>
2010-07-02 15:26:47 +0200

    Added yet another Log4J compatibility class.

---
+++ woodchipper/src/main/java/woodchipper/log4j/NDC.java
@@ -0,0 +1,19 @@
+package woodchipper.log4j;
+
+public class NDC {
+    
+	public static void push(String message) {
+	}
+
+	public static String pop() {
+		return "";
+	}
+
+	public static void remove() {
+	}
+
+	public static int getDepth() {
+		return 0;
+	}
+	
+}
